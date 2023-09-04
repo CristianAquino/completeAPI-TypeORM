ba3152bec22ee26d2d2f8d4c422ae3c060415190
cristianaquino <ycristian10@gmail.com>
2023-09-04 17:36:29 -0500

    feat: dbconfig

---
+++ src/db.ts
@@ -0,0 +1,15 @@
+import { DataSource } from "typeorm";
+import { User } from "./entities";
+
+const { HOST_DB, USERNAME_DB, PASSWORD_DB, DATABASE_DB, PORT_DB } = process.env;
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: HOST_DB as string,
+  port: Number(PORT_DB),
+  username: USERNAME_DB as string,
+  password: PASSWORD_DB as string,
+  database: DATABASE_DB as string,
+  entities: [User],
+  synchronize: true,
+});
