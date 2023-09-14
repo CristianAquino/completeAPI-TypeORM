2c240532366becc6aad6a01a226790be6038fbb3
cristianaquino <ycristian10@gmail.com>
2023-09-13 20:01:56 -0500

    feat: app

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,20 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import { authRoute, userRoute } from "./routes";
+import { handleError, logs } from "./middlewares";
 const app = express();
 
 // middlewars
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
+app.use(logs);
 
 // routes
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/user", userRoute);
 
 // errors
-
+app.use(handleError);
 export { app };
