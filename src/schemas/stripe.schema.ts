038faa89e04d6147b304f92ff31b828401e58459
cristianaquino <ycristian10@gmail.com>
2023-09-22 20:07:47 -0500

    feat: schema for products stripe

---
+++ src/schemas/stripe.schema.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+const productsSchema = z.object({
+  price: z.string().nonempty("identificador requerido"),
+  quantity: z
+    .number()
+    .nonnegative("debe ser un numero positivo")
+    .gt(0, "debe ser una cantidad mayor a 0"),
+});
+
+const checkoutSchema = z.array(productsSchema);
+
+export { checkoutSchema };
