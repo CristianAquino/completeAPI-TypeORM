abcbc3f92dee785367ff666c8674a1bf9f1bf36c
cristianaquino <ycristian10@gmail.com>
2023-09-22 20:07:25 -0500

    feat: create service checkout

--- src/services/stripe.service.ts
+++ src/services/stripe.service.ts
@@ -1,8 +1,9 @@
 import { Stripe } from "stripe";
+import { CheckoutType } from "../types";
 
 const { SECRET_KEY_STRIPE } = process.env;
 
-async function callAllProducts() {
+async function callAllPriceProducts() {
   const stripe = new Stripe(SECRET_KEY_STRIPE as string, {
     apiVersion: "2023-08-16",
   });
@@ -12,4 +13,19 @@ async function callAllProducts() {
   return prices;
 }
 
-export { callAllProducts };
+async function callCheckout(data: CheckoutType) {
+  const stripe = new Stripe(SECRET_KEY_STRIPE as string, {
+    apiVersion: "2023-08-16",
+  });
+  if (!stripe) throw new Error("NOT_FOUND");
+  const session = await stripe.checkout.sessions.create({
+    line_items: [...data],
+    mode: "subscription",
+    payment_method_types: ["card"],
+    success_url: "https://personal-portfolio-chi-sandy.vercel.app/",
+    cancel_url: "http://localhost:3000/",
+  });
+  if (!session) throw new Error("NOT_FOUND");
+  return { url: session.url };
+}
+export { callAllPriceProducts, callCheckout };
