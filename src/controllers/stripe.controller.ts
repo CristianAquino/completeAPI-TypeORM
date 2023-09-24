a67651cd7618f7505dd781b74f33df082de5604e
cristianaquino <ycristian10@gmail.com>
2023-09-24 17:27:43 -0500

    feat: stripe controller

--- src/controllers/stripe.controller.ts
+++ src/controllers/stripe.controller.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import Stripe from "stripe";
 import { callAllPriceProducts, callCheckout } from "../services";
 
+// prices
 async function allPrices(req: Request, res: Response, next: NextFunction) {
   try {
     const data = await callAllPriceProducts();
@@ -14,6 +15,44 @@ async function allPrices(req: Request, res: Response, next: NextFunction) {
   }
 }
 
+// products
+// async function createProduct(req: Request, res: Response, next: NextFunction) {
+//   try {
+//     const { body } = req;
+//     const response = await callCreateProduct(body);
+//     return res.status(200).json(response);
+//   } catch (error) {
+//     if (error instanceof Stripe.errors.StripeError) {
+//       return next(error.type);
+//     }
+//     return next(error);
+//   }
+// }
+// async function allProducts(req: Request, res: Response, next: NextFunction) {
+//   try {
+//     const response = await callAllProducts();
+//     return res.status(200).json(response);
+//   } catch (error) {
+//     if (error instanceof Stripe.errors.StripeError) {
+//       return next(error.type);
+//     }
+//     return next(error);
+//   }
+// }
+// async function productById(req: Request, res: Response, next: NextFunction) {
+//   try {
+//     const { id } = req.params;
+//     const response = await callProductById(id);
+//     return res.status(200).json(response);
+//   } catch (error) {
+//     if (error instanceof Stripe.errors.StripeError) {
+//       return next(error.type);
+//     }
+//     return next(error);
+//   }
+// }
+
+// checkout
 async function checkout(req: Request, res: Response, next: NextFunction) {
   try {
     const { body } = req;
@@ -27,4 +66,6 @@ async function checkout(req: Request, res: Response, next: NextFunction) {
   }
 }
 
-export { allPrices, checkout };
+// webhook
+
+export { checkout, allPrices };
