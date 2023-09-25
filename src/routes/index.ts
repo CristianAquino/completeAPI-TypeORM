da9bbed1d65ce157307c979122fe87719f4241cf
cristianaquino <ycristian10@gmail.com>
2023-09-25 17:44:39 -0500

    feat: update barrell

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,4 @@
 export * from "./auth.route";
 export * from "./user.route";
+export * from "./product.route";
 export * from "./stripe.route";
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,4 @@ export * from "./sendEmail.util";
 export * from "./signToken.util";
 export * from "./validateError.util";
 export * from "./validateErrorStripe.util";
+export * from "./slug.util";
