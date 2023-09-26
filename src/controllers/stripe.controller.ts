d5e0b830f5c5962e9c21c2fe552b442e4e108476
cristianaquino <ycristian10@gmail.com>
2023-09-25 23:26:14 -0500

    feat: stripe checkout controller

--- src/controllers/stripe.controller.ts
+++ src/controllers/stripe.controller.ts
@@ -1,56 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import Stripe from "stripe";
-import { callAllPriceProducts, callCheckout } from "../services";
-
-// prices
-async function allPrices(req: Request, res: Response, next: NextFunction) {
-  try {
-    const data = await callAllPriceProducts();
-    return res.status(200).json(data);
-  } catch (error) {
-    if (error instanceof Stripe.errors.StripeError) {
-      return next(error.type);
-    }
-    return next(error);
-  }
-}
-
-// products
-// async function createProduct(req: Request, res: Response, next: NextFunction) {
-//   try {
-//     const { body } = req;
-//     const response = await callCreateProduct(body);
-//     return res.status(200).json(response);
-//   } catch (error) {
-//     if (error instanceof Stripe.errors.StripeError) {
-//       return next(error.type);
-//     }
-//     return next(error);
-//   }
-// }
-// async function allProducts(req: Request, res: Response, next: NextFunction) {
-//   try {
-//     const response = await callAllProducts();
-//     return res.status(200).json(response);
-//   } catch (error) {
-//     if (error instanceof Stripe.errors.StripeError) {
-//       return next(error.type);
-//     }
-//     return next(error);
-//   }
-// }
-// async function productById(req: Request, res: Response, next: NextFunction) {
-//   try {
-//     const { id } = req.params;
-//     const response = await callProductById(id);
-//     return res.status(200).json(response);
-//   } catch (error) {
-//     if (error instanceof Stripe.errors.StripeError) {
-//       return next(error.type);
-//     }
-//     return next(error);
-//   }
-// }
+import { callCheckout } from "../services";
 
 // checkout
 async function checkout(req: Request, res: Response, next: NextFunction) {
@@ -67,5 +17,6 @@ async function checkout(req: Request, res: Response, next: NextFunction) {
 }
 
 // webhook
+function webhook(req: Request, res: Response, next: NextFunction) {}
 
-export { checkout, allPrices };
+export { checkout, webhook };
