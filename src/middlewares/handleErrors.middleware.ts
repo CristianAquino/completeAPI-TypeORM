8ccffbbfecf4ecf9c9a0761fb2b1f910d765eea8
cristianaquino <ycristian10@gmail.com>
2023-10-01 14:39:33 -0500

    feat: update

--- src/middlewares/handleErrors.middleware.ts
+++ src/middlewares/handleErrors.middleware.ts
@@ -8,18 +8,15 @@ function handleError(
   res: Response,
   next: NextFunction
 ) {
-  if (error instanceof Error) {
-    // const { code, message } = validationError(error.message);
-    // return res.status(code).send(message);
-    console.log(error);
-  }
-  if (error as Stripe.errors.StripeAPIError) {
-    // const { code, message } = validationErrorStripe(error);
-    // return res.status(code).send(message);
-    console.log(error);
-  }
-  console.log(error);
-  return res.status(500).send("SERVER ERROR");
+  // if (error instanceof Error) {
+  //   const { code, message } = validationError(error.message);
+  //   return res.status(code).send(message);
+  // }
+  // if (error as Stripe.errors.StripeAPIError) {
+  //   const { code, message } = validationErrorStripe(error);
+  //   return res.status(code).send(message);
+  // }
+  return res.status(500).send(error);
 }
 
 export { handleError };
