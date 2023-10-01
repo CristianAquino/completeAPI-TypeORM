2f4bde0c59408f883c2ffe25af7eb17c30839291
cristianaquino <ycristian10@gmail.com>
2023-09-30 22:10:31 -0500

    feat: update function sendEmail

--- src/utils/sendEmail.util.ts
+++ src/utils/sendEmail.util.ts
@@ -5,16 +5,16 @@ import path from "path";
 const { RESEND_KEY } = process.env;
 const resend = new Resend(RESEND_KEY);
 
-async function sendEmail(token: string) {
-  const data = await resend.emails.send({
+async function sendEmail(data: any, email: any, nameTemplate: string) {
+  const resendEmailSend = await resend.emails.send({
     from: "Acme <onboarding@resend.dev>",
-    to: ["ycristiantest@gmail.com"],
+    to: [email],
     subject: "Send Token",
     // html: `<a href=http://localhost:3000/${token}/new-password>Click for Change Password</a>`,
     html: await ejs.renderFile(
-      path.join(__dirname, "..", "templates", "sendEmail.ejs"),
+      path.join(__dirname, "..", "templates", `${nameTemplate}.ejs`),
       {
-        token,
+        data,
       }
     ),
     // attachments: [
@@ -24,7 +24,7 @@ async function sendEmail(token: string) {
     //   },
     // ],
   });
-  return data;
+  return resendEmailSend;
 }
 
 export { sendEmail };
