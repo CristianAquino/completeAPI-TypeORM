75f5ec91fd740cd793cdaf958213faec7f1d2be0
cristianaquino <ycristian10@gmail.com>
2023-10-01 14:45:50 -0500

    feat: update

--- src/schemas/stripe.schema.ts
+++ src/schemas/stripe.schema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 const productsSchema = z.object({
   id: z.string().nonempty("es necesario un identificador"),
   name: z.string().nonempty("es necesario el nombre del producto"),
-  marca: z.string().nonempty("es necesario la marca delproducto"),
+  brand: z.string().nonempty("es necesario la marca delproducto"),
   unitPrice: z.number().gt(0).default(1),
   description: z.string().default(""),
   image: z.string().nonempty("es necesario la imagen del producto"),
