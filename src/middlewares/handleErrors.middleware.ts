66770337fdd6be406897a0500314fc656653d369
cristianaquino <ycristian10@gmail.com>
2023-09-30 22:12:44 -0500

    feat: update errors

--- src/middlewares/handleErrors.middleware.ts
+++ src/middlewares/handleErrors.middleware.ts
@@ -9,14 +9,16 @@ function handleError(
   next: NextFunction
 ) {
   if (error instanceof Error) {
-    const { code, message } = validationError(error.message);
-    return res.status(code).send(message);
+    // const { code, message } = validationError(error.message);
+    // return res.status(code).send(message);
+    console.log(error);
   }
   if (error as Stripe.errors.StripeAPIError) {
-    const { code, message } = validationErrorStripe(error);
+    // const { code, message } = validationErrorStripe(error);
+    // return res.status(code).send(message);
     console.log(error);
-    return res.status(code).send(message);
   }
+  console.log(error);
   return res.status(500).send("SERVER ERROR");
 }
 
